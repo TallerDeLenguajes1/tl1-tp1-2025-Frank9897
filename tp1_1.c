38d8fe8317a86502a79f83285e9af43e1584612c
Frank9897 <franco.vasquez9897@gmail.com>
2025-04-01 02:07:09 -0300

    Agregando consigna de punteros

--- tp1_1.c
+++ tp1_1.c
@@ -2,6 +2,13 @@
 
 int main()
 {
-    printf("Hola Mundo");
+    int num=5;
+    int *pnum =&num;
+    printf("\nEl contenido del puntero: %d",*pnum);
+    printf("\nLa direccion de memoria almacenada por el puntero: %p",pnum);
+    printf("\nla direccion de memoria de la variable: %p",&num);
+    printf("\nla direccion de memoria del puntero: %p",&pnum);
+    printf("\nTamanio de memoria utilizado por esa variable: %d bytes",sizeof(num));
+
     return 0;
 }
