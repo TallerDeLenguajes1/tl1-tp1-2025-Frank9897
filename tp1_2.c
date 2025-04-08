1a5ee57c2588b8e3965734565dd8d4a995875229
Frank9897 <franco.vasquez9897@gmail.com>
2025-04-07 22:58:09 -0300

    Codificacion de apartado a)

--- tp1_2.c
+++ tp1_2.c
@@ -5,10 +5,13 @@ int devolvercuadrado(int n);
 
 int main()
 {
+    int n = 4,ncuadrado;
+    ncuadrado=devolvercuadrado(n);
+    printf("El cuadrado de %d es %d",n,ncuadrado);
     return 0;
 }
 
 int devolvercuadrado(int n)
 {
-    
+    return n*n;
 }
