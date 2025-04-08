f9f67cf26f8b4342c523f1c3c9a8703728e90988
Frank9897 <franco.vasquez9897@gmail.com>
2025-04-07 23:20:45 -0300

    Codificacion de apartado e)

--- tp1_2.c
+++ tp1_2.c
@@ -5,16 +5,28 @@ void invertir(int a, int b);
 
 int main()
 {
-    int a = 4,b = 8;
+    int a = 8,b = 8;
     invertir(a,b);
     return 0;
 }
 
 void invertir(int a, int b)
 {
-    printf("Numero A: %d\nNumero B: %d",a,b);
-    int aux = b;
-    b=a;
-    a=aux;
-    printf("\nInvertir...\nNumero A: %d\nNumero B: %d",a,b);
+    int menor=0,mayor=0;
+    if (a<b)
+    {
+        menor=a;
+        mayor=b;
+        printf("Numero menor: %d\nNumero mayor: %d",menor,mayor);
+    }else if (b<a)
+    {
+        menor=b;
+        mayor=a;
+        printf("Numero menor: %d\nNumero mayor: %d",menor,mayor);
+    }else
+    {
+        menor=a;
+        mayor=b;
+        printf("Numero menor: %d\nNumero mayor: %d",menor,mayor);
+    }
 }
