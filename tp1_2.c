71c98f3c26fbac6cb470e545ba60128c51da0b20
Frank9897 <franco.vasquez9897@gmail.com>
2025-04-07 23:50:51 -0300

    Codificacion apartado f)

--- tp1_2.c
+++ tp1_2.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void invertir(int a, int b);
 void orden(int a, int b);
 
 int main()
 {
-    int a = 8,b = 8;
+    int a,b;
+    printf("Ingrese por pantalla dos numeros\nNumero 1: ");
+    scanf("%d", &a);
+    printf("\nNumero 2: ");
+    scanf("%d", &b);
+    invertir(a,b);
     orden(a,b);
     return 0;
 }
 
+
+void invertir(int a, int b)
+{
+    printf("Numero A: %d\nNumero B: %d",a,b);
+    int aux = b;
+    b=a;
+    a=aux;
+    printf("\nInvertir...\nNumero A: %d\nNumero B: %d\n",a,b);
+}
 void orden(int a, int b)
 {
     int menor=0,mayor=0;
@@ -17,7 +32,7 @@ void orden(int a, int b)
     {
         menor=a;
         mayor=b;
-        printf("Numero menor: %d\nNumero mayor: %d",menor,mayor);
+        printf("\nNumero menor: %d\nNumero mayor: %d",menor,mayor);
     }else if (b<a)
     {
         menor=b;
