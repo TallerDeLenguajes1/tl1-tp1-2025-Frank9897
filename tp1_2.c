55b84ad459fe178d63ee9cd21272ae084e6649cd
Frank9897 <franco.vasquez9897@gmail.com>
2025-04-07 23:02:52 -0300

    Definicion y codificacion de funcion del apartado b)

--- tp1_2.c
+++ tp1_2.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int devolvercuadrado(int n);
+void devolvercuadrado(int n);
 
 int main()
 {
-    int n = 4,ncuadrado;
-    ncuadrado=devolvercuadrado(n);
-    printf("El cuadrado de %d es %d",n,ncuadrado);
+    int n = 4;
+    devolvercuadrado(n);
     return 0;
 }
 
-int devolvercuadrado(int n)
+void devolvercuadrado(int n)
 {
-    return n*n;
+    int ncuadrado= n*n;
+    printf("El cuadrado de %d es %d",n,ncuadrado);
 }
